80e3cba87ba07a597cefa7ad11a95df98419e3eb
Aditi Chegu <achegu@uw.edu>
2026-07-16 10:52:58 -0700

    Update _model.py

--- src/mesh_annot/_model.py
+++ src/mesh_annot/_model.py
@@ -1,1 +1,26 @@
+import torch
+import torch.nn.functional as F
 
+from torch_geometric.utils import to_undirected
+from torch_geometric.utils import SplineConv
+
+class SplineGCN(torch.nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv1 = SplineConv(in_channels, 16, dim=3, kernel_size=5)
+        self.conv2 = SplineConv(16, 32, dim=3, kernel_size=5)
+        self.conv3 = SplineConv(32, 16, dim=3, kernel_size=5)
+        self.conv4 = SplineConv(16, out_channels, dim=3, kernel_size=5)
+    def forward(self, data):
+        x, edge_index, pseudo = data.x, data.edge_index, data.edge_attr
+
+        x = self.conv1(x, edge_index, pseudo)
+        x = F.relu(x)
+        x = self.conv2(x, edge_index, pseudo)
+        x = F.relu(x)
+        x = self.conv3(x, edge_index, pseudo)
+        x = F.relu(x)
+        x = self.conv4(x, edge_index, pseudo)
+        x = F.relu(x)
+
+        return x
