096ba247f4c6eb4d1b94c327feedd8c19a6fa6e8
Aditi Chegu <achegu@uw.edu>
2026-07-17 10:22:32 -0700

    Update _model.py

--- src/mesh_annot/_model.py
+++ src/mesh_annot/_model.py
@@ -24,3 +24,17 @@ class SplineGCN(torch.nn.Module):
         x = F.relu(x)
 
         return x
+
+# class ExampleGCN(torch.nn.Module):
+#     def __init__(self, in_channels, out_channels):
+#         super().__init__()
+#         self.conv1 = GCNConv(in_channels, 16)
+#         self.conv2 = GCNConv(16, out_channels)
+
+#     def forward(self, data):
+#         x, edge_index = data.x, data.edge_index
+#         x = self.conv1(x, edge_index)
+#         x = F.relu(x)
+#         x = self.conv2(x, edge_index)
+
+#         return x
