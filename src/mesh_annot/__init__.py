f95d8df2fbe9adb76df3a90264f37b479ba92f88
Aditi Chegu <achegu@uw.edu>
2026-07-21 12:31:05 -0700

    Update __init__.py

--- src/mesh_annot/__init__.py
+++ src/mesh_annot/__init__.py
@@ -1,7 +1,10 @@
 from ._model import SplineGCN
+from ._data import HCPDataset
 
 SplineGCN.__module__ = __name__
+HCPDataset.__module__ = __name__
 
 def reload():
     import sys, importlib
     importlib.reload(sys.modules['mesh_annot._model'])
+    importlib.reload(sys.modules['mesh_annot._data'])
