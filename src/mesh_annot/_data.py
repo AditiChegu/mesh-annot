a02d023a7c3ffaad898f4b0a377f0573dabdfd60
Aditi Chegu <achegu@uw.edu>
2026-07-23 14:20:15 -0700

    Update _data.py

--- src/mesh_annot/_data.py
+++ src/mesh_annot/_data.py
@@ -1,11 +1,34 @@
-import neuropythy as ny
+import os
 from pathlib import Path
+
+import numpy as np
+import neuropythy as ny
+from . import config as cfg
+
 import torch
 import torch_geometric
-from . import config as cfg
-import numpy as np
+from torch_geometric.utils import to_undirected
+from torch_geometric.data import Data
+
 
 class HCPDataset(torch_geometric.data.Dataset):
+    def create_graph(self, vertices, edges):
+        if edges.shape[0] == 2 and edges.shape[1] != 2:
+            edges = edges.T
+
+        edge_index = torch.tensor(edges.T, dtype=torch.long)
+        edge_index = to_undirected(edge_index)
+        x_coords = torch.tensor(vertices.T, dtype=torch.long)
+
+        src = x_coords[edge_index[0]]
+        trg = x_coords[edge_index[1]]
+        edge_attr = src - trg
+        edge_attr = (edge_attr - edge_attr.min()) / (edge_attr.max() - edge_attr.min())
+
+        graph_data = Data(x=x_coords, edge_index=edge_index, edge_attr=edge_attr)
+        
+        return graph_data
+        
     @staticmethod
     def _prop_list(base_path):
         return [
@@ -56,6 +79,28 @@ class HCPDataset(torch_geometric.data.Dataset):
         nsids = len(sids)
         nraters = len(raters)
         dset_dims = (nraters * nsids, nprops, 163842)
+
+        self.lh_graphs = {}
+        self.rh_graphs = {}
+        for sid in sids:
+            lh_mesh_path = os.path.join(self.base_path, f"{sid}.lh.mesh")
+            rh_mesh_path = os.path.join(self.base_path, f"{sid}.rh.mesh")
+
+            lh_mesh = ny.load(lh_mesh_path, 'freesurfer_geometry')
+            rh_mesh = ny.load(rh_mesh_path, 'freesurfer_geometry')
+            
+            lh_vertices = lh_mesh.coordinates
+            rh_vertices = rh_mesh.coordinates
+            
+            lh_edges = lh_mesh.tess.edges
+            rh_edges = rh_mesh.tess.edges
+
+            lh_graph = self.create_graph(lh_vertices, lh_edges)
+            rh_graph = self.create_graph(rh_vertices, rh_edges)
+
+            self.lh_graphs[sid] = lh_graph
+            self.rh_graphs[sid] = rh_graph
+            print("Saved:", sid)
 
         self.data = torch.zeros(dset_dims)
         ii = 0
