6b7aab3bdb57eac579b33ad3774a23882d70e30a
Aditi Chegu <achegu@uw.edu>
2026-08-04 09:55:04 -0700

    Update _train.py

--- src/mesh_annot/_train.py
+++ src/mesh_annot/_train.py
@@ -3,10 +3,11 @@ from pathlib import Path
 from collections import namedtuple
 
 import json
+import pandas as pd
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import pandas as pd
+from torch_geometric.loader import DataLoader
 
 from ._model import SplineGCN
 from . import config as cfg
@@ -49,9 +50,39 @@ def trn_epoch(
         loss = criterion(output, batch.y)
         loss_total += loss.detach().item()
         
-    batch_size = len(loader)
+    n = len(loader)
     return {
-        'total loss' : loss_total / n
+        'total' : loss_total / n
+    }
+
+@torch.no_grad()
+@cfg.wrap_opts
+def val_epoch(
+    epochno,
+    /,
+    model,
+    loader,
+    optimizer,
+    *,
+    noise_std=Ellipsis,
+    device=Ellipsis
+):
+    model.eval()
+    loss_total = 0.0
+    # TODO Check if there is a better place to intialize these values
+    lr = 0.01
+    optimizer = optim.Adam(model.parameters(), lr=0.01)
+    criterion = torch.nn.MSELoss()
+
+    for batch in loader:
+        batch = batch.to(device)
+        output = model(add_noise(batch, noise_std=noise_std))
+        loss = criterion(output, batch.y)
+        loss_total += loss.detach().item()
+
+    n = len(loader)
+    return {
+        'total' : loss_total / n 
     }
 
 # TRAINING RUNS
@@ -189,6 +220,9 @@ def train(
     dropout=Ellipsis,
     device=Ellipsis,
     lr=Ellipsis,
+    lr_decay=Ellipsis,
+    lr_patience=Ellipsis,
+    lr_min=Ellipsis,
     noise_std=Ellipsis,
     target=Ellipsis,
     hemisphere=Ellipsis,
@@ -210,5 +244,124 @@ def train(
     history_filename='history.tsv'
 ):
     from . import _train as self
-    # TODO Finish this function
-    
+    # If we are resuming, we need to overwrite all the hyperparams
+    if resume is not None:
+        loc = locals()
+        for (k, v) in cfg.hyperparams(resume.hyperparams).items():
+            if k in loc:
+                loc[k] = v
+        # The inital model must be the run's model
+        initmodel = resume.model
+        hyperparams = resume.hyperparams
+
+    # Build the model
+    if initmodel is None:
+        model = self.model(
+            properties=properties,
+            target=target,
+            dropout=dropout,
+            device=device
+        )
+    else:
+        model = initmodel.to(device)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    # Learning rate scheduler
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+        optimizer,
+        mode='min',
+        factor=lr_decay,
+        patience=lr_patience,
+        min_lr=lr_min
+    )
+
+    trn_loader = DataLoader(
+        trn_dset,
+        batch_size=batch_size,
+        shuffle=True
+    )
+
+    # TODO Check if this needs to be changed
+    val_loader = DataLoader(
+        val_dset,
+        batch_size=len(val_dset),
+        shuffle=False
+    )
+
+    # MAIN TRAINING LOOP
+    best_val = float('inf')
+    best_mdl = None
+    history = []
+    if resume is None:
+        epochnos=range(epochs)
+    else:
+        for (ii, row) in resume.history.iterrows():
+            scheduler.step(row['val_total'])
+        history = resume.history.to_dict(orient='records')
+        nhist=len(history)
+        epochnos= range(nhist, nhist + epochs)
+
+    for epochno in epochnos:
+        step_lr = optimizer.param_groups[0]['lr']
+        trn_losses = trn_epoch(
+            epochno,
+            model,
+            trn_loader,
+            optimizer,
+            noise_std=noise_std,
+            device=device
+        )
+        val_losses = val_epoch(
+            epochno,
+            model,
+            val_loader,
+            optimizer,
+            noise_std=noise_std,
+            device=device
+        )
+
+        if torch.isnan(torch.tensor(val_losses['total'])):
+            raise RunTimeError(f'NaN loss value on epoch {epochno}')
+        scheduler.step(val_losses['total'])
+        if (epochno + 1) % 10 == 0 and log is not None:
+            log (
+                f'Epoch {epochno + 1: 04d} || '
+                f'LR {step_lr*1e4: 8.6f}e-4 || '
+                f'trn {trn_losses['total']: <9.3f} || '
+                f'val {val_losses['total']: <9.3f}'
+            )
+            if val_losses['total'] < best_val:
+                best_val = val_losses['total']
+                best_mdl = model.state_dict()
+                saved = True
+            else:
+                saved = False
+
+            hentry = {
+                'epoch': epochno,
+                'lr': step_lr
+            }
+            for (k, v) in trn_losses.items():
+                hentry[f'trn_{k}'] = v
+            for (k, v) in val_losses.items():
+                hentry[f'val_{k}'] = v
+            hentry['saved'] = saved
+            history.append(hentry)
+
+        history = pd.DataFrame(history)
+        model.load_state_dict(best_mdl)
+
+        # Make a run object to save the model, if requested
+        run = Run(hyperparams, model, history)
+        if savedir is not None:
+            run.save(
+                savedir,
+                mkdir=mkdir,
+                model_filename=model_filename,
+                hyperparams_filename=hyperparams_filename,
+                history_filename=history_filename
+            )
+        if loss_only:
+            return best_val
+        else:
+            return run
